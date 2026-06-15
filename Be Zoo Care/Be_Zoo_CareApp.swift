827bed9103b37607ed14b28724acf8ed3db6f152
Tymur <batulintimur@gmail.com>
2026-06-15 14:49:41 +0200

    Initial Commit

---
+++ Be Zoo Care/Be_Zoo_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Be_Zoo_CareApp.swift
+//  Be Zoo Care
+//
+//  Created by Tymur on 15.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Be_Zoo_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Be Zoo Care/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Be Zoo Care
+//
+//  Created by Tymur on 15.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
